1c7d602be18dcb86dbe01178a12d934f4c6265a8
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-09-28 13:57:27 -0500

    New music

--- Assets/Scripts/PlayGameMusic.cs
+++ Assets/Scripts/PlayGameMusic.cs
@@ -7,35 +7,55 @@ public class PlayGameMusic : MonoBehaviour
     // Controls the playing of music in game
 
     [SerializeField]
-    private AudioSource song;
+    private AudioSource[] songs;
 
     [SerializeField]
     private Player player;
 
+    private int currentSong;
+
+    private bool songStarted;
+
     // Update is called once per frame
     private void Start() 
     {
+        foreach(AudioSource song in songs)
         song.volume = 0;
+
+        currentSong = 0;
+
+        songStarted = false;
     }
 
     void FixedUpdate()
     {
-        if((player.tilesGenerated % 500 == 250 || player.tilesGenerated % 500 == 251 || player.tilesGenerated % 500 == 252) && !song.isPlaying) StartCoroutine(PlaySong());
+        if((player.tilesGenerated % 350 == 250 || player.tilesGenerated % 350 == 251 || player.tilesGenerated % 350 == 252) && !songs[currentSong].isPlaying) 
+        {
+            StartCoroutine(PlaySong(currentSong));
+        }
+
+        if(songStarted && !songs[currentSong].isPlaying)
+        {
+            songStarted = false;
+            if(currentSong < songs.Length - 1) currentSong++;
+            else currentSong = 0;
+        }
     }
 
-    private IEnumerator PlaySong()
+    private IEnumerator PlaySong(int songNumber)
     {
-        song.Stop();
-        song.Play();
-        song.volume = 0;
+        songStarted = true;
+        songs[songNumber].Stop();
+        songs[songNumber].Play();
+        songs[songNumber].volume = 0;
 
-        while(song.volume < .99)
+        while(songs[songNumber].volume < .99)
         {
             yield return new WaitForSeconds(.6f);
-            song.volume += .125f;
+            songs[songNumber].volume += .125f;
         }
 
         yield return new WaitForSeconds(.1f);
-        StopCoroutine(PlaySong());
+        StopCoroutine(PlaySong(songNumber));
     }
 }
